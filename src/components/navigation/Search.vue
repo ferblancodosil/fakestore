11a1c62dfa20d37eb076691353110524f8e1a30a
blain <blain@argalladas.eu>
2021-03-13 12:57:54 +0100

    Create search component

--- src/components/navigation/Search.vue
+++ src/components/navigation/Search.vue
@@ -1,11 +1,44 @@
 <template>
-  <div class="grid_product">
-    Search
-  </div>
+  <form class="search" @submit.prevent="search">
+    <img src="~@/assets/images/search.svg">
+    <input v-model="input" type="text" class="input" placeholder="Buscar Producto">
+  </form>
 </template>
 
 <script>
-  export default {
-    name: 'Search'
+export default {
+  name: 'Search',
+  data() {
+    return {
+      input: ''
+    }
+  },
+  methods: {
+    search() {
+      this.$emit('onSearch', { value: this.input })
+    }
   }
+}
 </script>
+<style lang="scss" scoped>
+  .search {
+    width: 100%;
+    position: relative;
+    font-weight: 300;
+    font-size: 14px;
+    line-height: 18px;
+    img {
+      position: absolute;
+      left: 11px;
+      top: 11px;
+    }
+    .input {
+      width: calc(100% - 34px);
+      background: #F1F3F5;
+      padding: 11px 0px 11px 34px;
+      border: 0;
+      border-radius: 10px;
+    }
+  }
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="Home">
+    <search class="search" @onSearch="search"></search>
     <h3>Hello World!</h3>
   </div>
 </template>
 
 <script>
+import Search from '@/components/navigation/Search'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Search
+  },
+  methods: {
+    search({ value }) {
+      // eslint-disable-next-line
+      alert(`Vamos a buscar el valor ${value}`)
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+  .search {
+    margin: 8px 0px;
+  }
 </style>
